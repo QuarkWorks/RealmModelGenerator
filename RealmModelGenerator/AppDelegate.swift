4fd10d7f52357730878286fb48b5a32e3bc9fac5
Brandon Erbschloe <berbschloe@gmail.com>
2016-03-02 18:56:11 -0600

    Initial Commit

---
+++ RealmModelGenerator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RealmModelGenerator
+//
+//  Created by Brandon Erbschloe on 3/2/16.
+//  Copyright © 2016 QuarkWorks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RealmModelGenerator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RealmModelGenerator
+//
+//  Created by Brandon Erbschloe on 3/2/16.
+//  Copyright © 2016 QuarkWorks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
