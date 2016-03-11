a27bdce543ccb020093af6793ae12c260935c62c
berbschloe <berbschloe@gmail.com>
2016-03-11 03:15:22 -0600

    Add Title Cell and Nib Helper

---
+++ RealmModelGenerator/NibDesignable.swift
@@ -0,0 +1,108 @@
+import Cocoa
+
+public protocol NibDesignableProtocol: NSObjectProtocol {
+    /**
+     Identifies the view that will be the superview of the contents loaded from
+     the Nib. Referenced in setupNib().
+     
+     - returns: Superview for Nib contents.
+     */
+    var nibContainerView: NSView { get }
+    // MARK: - Nib loading
+    
+    /**
+    Called to load the nib in setupNib().
+    
+    - returns: NSView instance loaded from a nib file.
+    */
+    func loadNib() -> NSView
+    /**
+     Called in the default implementation of loadNib(). Default is class name.
+     
+     - returns: Name of a single view nib file.
+     */
+    func nibName() -> String
+    
+    func nibDidInitialize()
+}
+
+extension NibDesignableProtocol {
+    // MARK: - Nib loading
+    
+    /**
+    Called to load the nib in setupNib().
+    
+    - returns: NSView instance loaded from a nib file.
+    */
+    public func loadNib() -> NSView {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = NSNib(nibNamed: self.nibName(), bundle: bundle)
+        var objects:NSArray?
+        nib!.instantiateWithOwner(self, topLevelObjects: &objects)
+        return objects?.filter({$0 is NSView}).first as! NSView
+    }
+    
+    // MARK: - Nib loading
+    
+    /**
+    Called in init(frame:) and init(aDecoder:) to load the nib and add it as a subview.
+    */
+    private func setupNib() {
+        let view = self.loadNib()
+        self.nibContainerView.addSubview(view)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        let bindings = ["view": view]
+        self.nibContainerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|[view]|", options:[], metrics:nil, views: bindings))
+        self.nibContainerView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|[view]|", options:[], metrics:nil, views: bindings))
+        nibDidInitialize()
+    }
+}
+
+extension NSView {
+    public var nibContainerView: NSView {
+        return self
+    }
+    /**
+     Called in the default implementation of loadNib(). Default is class name.
+     
+     - returns: Name of a single view nib file.
+     */
+    public func nibName() -> String {
+        return self.dynamicType.description().componentsSeparatedByString(".").last!
+    }
+    
+    public func nibDidInitialize() {}
+}
+
+@IBDesignable
+public class NibDesignableView: NSView, NibDesignableProtocol {
+    
+    // MARK: - Initializer
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupNib()
+    }
+    
+    // MARK: - NSCoding
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupNib()
+    }
+}
+
+@IBDesignable
+public class NibDesignableTableCellView: NSTableCellView, NibDesignableProtocol {
+    // MARK: - Initializer
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupNib()
+    }
+    
+    // MARK: - NSCoding
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupNib()
+    }
+}
---
+++ RealmModelGenerator/TitleCell.swift
@@ -0,0 +1,68 @@
+//
+//  EntityCell.swift
+//  RealmModelGenerator
+//
+//  Created by Brandon Erbschloe on 3/10/16.
+//  Copyright © 2016 QuarkWorks. All rights reserved.
+//
+
+import Cocoa
+import Quartz
+
+@IBDesignable
+class TitleCell: NibDesignableTableCellView, NSTextFieldDelegate {
+
+    @IBOutlet var titleTextField:NSTextField! {
+        willSet {
+            newValue!.delegate = self
+        }
+    }
+    @IBOutlet var letterTextField:NSTextField!
+    
+    override func nibDidInitialize() {
+        Swift.print("\(__FUNCTION__)")
+        self.wantsLayer = true
+    }
+    
+    @IBInspectable var letterColor:NSColor? {
+        set {
+            self.letterTextField.backgroundColor = newValue
+        }
+
+        get {
+            return self.letterTextField.backgroundColor
+        }
+    }
+    
+    @IBInspectable var title:String {
+        set {
+            self.titleTextField.stringValue = newValue
+        }
+        
+        get {
+            return self.titleTextField.stringValue
+        }
+    }
+    
+    @IBInspectable var letter:String {
+        set {
+            self.letterTextField.stringValue = newValue
+        }
+
+        get {
+            return self.letterTextField.stringValue
+        }
+    }
+    
+    func control(control: NSControl, textShouldBeginEditing fieldEditor: NSText) -> Bool {
+        Swift.print("\(__FUNCTION__)")
+        self.layer!.backgroundColor = NSColor.blueColor().CGColor
+        return true
+    }
+    
+    func control(control: NSControl, textShouldEndEditing fieldEditor: NSText) -> Bool {
+        Swift.print("\(__FUNCTION__)")
+        self.layer!.backgroundColor = NSColor.redColor().CGColor
+        return true
+    }
+}
