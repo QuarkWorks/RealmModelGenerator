8f78eca2b6e2d4ff239041746828bda56b1010e3
James Durham <jamesldurham@gmail.com>
2018-03-16 13:27:47 -0500

    add MainWindow bug fix for xcode 9.x

---
+++ RealmModelGenerator/MainWindow.swift
@@ -0,0 +1,13 @@
+//
+//  MainWindow.swift
+//  RealmModelGenerator
+//
+//  Created by James Durham on 3/16/18.
+//  Copyright © 2018 QuarkWorks. All rights reserved.
+//
+
+import Cocoa
+// Bug fix for xcode 9.x
+class MainWindow: NSWindow {
+
+}
